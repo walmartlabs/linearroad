b8131cf56513de7ec0fced38b6d22b3130335485
shkdev <shkhomes@gmail.com>
2015-10-24 00:52:01 -0500

    Create historical-tolls.pl
    This is the original file from the data generator of the tools page at http://www.cs.brandeis.edu/~linearroad/tools.html

---
+++ historical-tolls.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -w
+
+@ARGV == 3 or die("to generate toll-history, give me # of xways, maxcarid");
+
+my $xway = $ARGV[0];
+my $max_xway0 =$ARGV[1];
+my $dir=$ARGV[2];
+
+#open(OUT, ">$dir/xway$xway.historical-tolls");
+# Put all historical tolls of different expressway in 1 files
+open(OUT, ">$dir/historical-tolls.out");
+
+
+for (my $vid = 1; $vid <= $max_xway0; ++$vid) {
+    for (my $day = 1; $day <= 69; ++$day) {
+        my $toll = int(rand(99));
+        $xway1 = int(rand ($xway));
+        # (vid, day, xway, tolls)
+        print OUT "$vid,$day,$xway1,$toll\n";
+    }
+}
+close (OUT);
